407885619dccc6d8f1825a60d6d49a31606639e0
kritarthaa <akritartha@gmail.com>
2019-12-03 15:48:50 +0545

    changed

--- e2e/User/userDashboard/userdashboard.e2e-spec.ts
+++ e2e/User/userDashboard/userdashboard.e2e-spec.ts
@@ -1,44 +1,44 @@
-import { AppPage } from '.app';
+import { AppPage } from '../../app.po';
 import { browser, ExpectedConditions, Ptor } from 'protractor';
 import { async } from 'q';
 
-fdescribe('User Dashboard', () => {
-let app = new AppPage();
+describe('User Dashboard', () => {
+    let app = new AppPage();
 
-        beforeEach(async () => {
-            app = new AppPage();
-            await app.login.navigateTo();
-            await app.login.setTenantID(app.User[0].tenant);
-            await app.login.setAdminUsername(app.User[0].Username);
-            await app.login.setAdminPassword(app.User[0].Password);
-            await app.login.clickAdminLoginBtn();
+    beforeEach(async () => {
+        app = new AppPage();
+        await app.login.navigateTo();
+        await app.login.setTenantID(app.User[0].tenant);
+        await app.login.setAdminUsername(app.User[0].Username);
+        await app.login.setAdminPassword(app.User[0].Password);
+        await app.login.clickAdminLoginBtn();
 
-});      
+    });
 
-        // Click on the hamburger icon/menu icon
+    // Click on the hamburger icon/menu icon
 
-        it('Click on Hamburger Icon ', async () => {
-            await browser.waitForAngularEnabled(false);
-            await(browser.wait(ExpectedConditions.urlContains("/dashboard"),10000));
-            await(browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.menuIcon)));
-            await(app.dashboardpage.menuIcon.click());                  
-        });
+    it('Click on Hamburger Icon ', async () => {
+        await browser.waitForAngularEnabled(false);
+        await (browser.wait(ExpectedConditions.urlContains("/dashboard"), 10000));
+        await (browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.menuIcon)));
+        await (app.dashboardpage.menuIcon.click());
+    });
 
 
-        // Click on the tenant option
+    // Click on the tenant option
 
-        it('Click on tenants section', async() => {
+    it('Click on tenants section', async () => {
 
-            await browser.waitForAngularEnabled(false);
-            await(browser.wait(ExpectedConditions.urlContains("/dashboard"),10000));
-            
-            await(browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.tenants),10000));
-            await(app.dashboardpage.tenants.click());
-            
-            await(browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.addTenantbtn), 10000));
-            await(app.dashboardpage.addTenantbtn.click());
+        await browser.waitForAngularEnabled(false);
+        await (browser.wait(ExpectedConditions.urlContains("/dashboard"), 10000));
 
-            
-        });
+        await (browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.tenants), 10000));
+        await (app.dashboardpage.tenants.click());
+
+        await (browser.wait(ExpectedConditions.visibilityOf(app.dashboardpage.addTenantbtn), 10000));
+        await (app.dashboardpage.addTenantbtn.click());
+
+
+    });
 
 });
